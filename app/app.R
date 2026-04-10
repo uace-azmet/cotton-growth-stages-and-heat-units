3f9a7fc552eba2c804a705ed3d5ae545c6672fac
Jeremy Weiss <jlweiss@email.arizona.edu>
2026-04-10 12:41:14 -0700

    `sidebar` CSS

--- app/app.R
+++ app/app.R
@@ -17,12 +17,11 @@ ui <-
         bslib::layout_sidebar(
           sidebar = sidebar, # `scr##_sidebar.R`
           
-          shiny::htmlOutput(outputId = "figureTitle"),
-          shiny::htmlOutput(outputId = "figureSummary"),
-          shiny::htmlOutput(outputId = "figureHelpText"),
-          #shiny::plotOutput(outputId = "figure"),
-          plotly::plotlyOutput(outputId = "figure"),
-          shiny::htmlOutput(outputId = "figureFooter")
+          # shiny::htmlOutput(outputId = "figureTitle"),
+          # shiny::htmlOutput(outputId = "figureSummary"),
+          # shiny::htmlOutput(outputId = "figureHelpText"),
+          # plotly::plotlyOutput(outputId = "figure"),
+          # shiny::htmlOutput(outputId = "figureFooter")
         ) |>
           htmltools::tagAppendAttributes(
             #https://getbootstrap.com/docs/5.0/utilities/api/
@@ -43,7 +42,7 @@ server <- function(input, output, session) {
   # Observables -----
   
   shiny::observeEvent(input$calculateHeatUnits, {
-    if (input$plantingDate > input$endDate) {
+    if (input$startDate > input$endDate) {
       shiny::showModal(datepickerErrorModal) # `scr##_datepickerErrorModal.R`
     }
   })
@@ -51,113 +50,113 @@ server <- function(input, output, session) {
   
   # Reactives -----
   
-  dataMerge <- 
-    shiny::eventReactive(input$calculateHeatUnits, {
-      shiny::validate(
-        shiny::need(
-          expr = input$plantingDate <= input$endDate, 
-          message = FALSE
-        )
-      )
-    
-    idCalculatingHeatUnits <- 
-      shiny::showNotification(
-        ui = "Calculating heat units . . .", 
-        action = NULL, 
-        duration = NULL, 
-        closeButton = FALSE,
-        id = "idCalculatingHeatUnits",
-        type = "message"
-      )
-    
-    on.exit(removeNotification(id = idCalculatingHeatUnits), add = TRUE)
-    
-    fxn_dataMerge( # Calls 'fxn_dataELT()' and 'fxn_dataHeatSum()'
-      azmetStation = input$azmetStation, 
-      startDate = input$plantingDate, 
-      endDate = input$endDate
-    )
-  })
+  # dataMerge <- 
+  #   shiny::eventReactive(input$calculateHeatUnits, {
+  #     shiny::validate(
+  #       shiny::need(
+  #         expr = input$plantingDate <= input$endDate, 
+  #         message = FALSE
+  #       )
+  #     )
+  #   
+  #   idCalculatingHeatUnits <- 
+  #     shiny::showNotification(
+  #       ui = "Calculating heat units . . .", 
+  #       action = NULL, 
+  #       duration = NULL, 
+  #       closeButton = FALSE,
+  #       id = "idCalculatingHeatUnits",
+  #       type = "message"
+  #     )
+  #   
+  #   on.exit(removeNotification(id = idCalculatingHeatUnits), add = TRUE)
+  #   
+  #   fxn_dataMerge( # Calls 'fxn_dataELT()' and 'fxn_dataHeatSum()'
+  #     azmetStation = input$azmetStation, 
+  #     startDate = input$plantingDate, 
+  #     endDate = input$endDate
+  #   )
+  # })
   
-  figure <- 
-    shiny::eventReactive(dataMerge(), {
-      fxn_figure(
-        inData = dataMerge(),
-        azmetStation = input$azmetStation
-      )
-    })
-  
-  figureFooter <- 
-    shiny::eventReactive(dataMerge(), {
-      fxn_figureFooter(
-        azmetStation = input$azmetStation,
-        startDate = input$plantingDate, 
-        endDate = input$endDate
-      )
-    })
-  
-  figureHelpText <- 
-    shiny::eventReactive(dataMerge(), {
-      fxn_figureHelpText()
-    })
-  
-  figureSummary <- 
-    shiny::eventReactive(dataMerge(), {
-      fxn_figureSummary(
-        azmetStation = input$azmetStation, 
-        inData = dataMerge(),
-        startDate = input$plantingDate, 
-        endDate = input$endDate
-      )
-    })
-  
-  figureTitle <- 
-    shiny::eventReactive(dataMerge(), {
-      fxn_figureTitle(azmetStation = input$azmetStation)
-    })
-  
-  pageSupportText <- 
-    shiny::eventReactive(dataMerge(), {
-      fxn_pageSupportText(
-        azmetStation = input$azmetStation,
-        startDate = input$plantingDate, 
-        endDate = input$endDate, 
-        timeStep = "Daily"
-      )
-    })
+  # figure <- 
+  #   shiny::eventReactive(dataMerge(), {
+  #     fxn_figure(
+  #       inData = dataMerge(),
+  #       azmetStation = input$azmetStation
+  #     )
+  #   })
+  # 
+  # figureFooter <- 
+  #   shiny::eventReactive(dataMerge(), {
+  #     fxn_figureFooter(
+  #       azmetStation = input$azmetStation,
+  #       startDate = input$plantingDate, 
+  #       endDate = input$endDate
+  #     )
+  #   })
+  # 
+  # figureHelpText <- 
+  #   shiny::eventReactive(dataMerge(), {
+  #     fxn_figureHelpText()
+  #   })
+  # 
+  # figureSummary <- 
+  #   shiny::eventReactive(dataMerge(), {
+  #     fxn_figureSummary(
+  #       azmetStation = input$azmetStation, 
+  #       inData = dataMerge(),
+  #       startDate = input$plantingDate, 
+  #       endDate = input$endDate
+  #     )
+  #   })
+  # 
+  # figureTitle <- 
+  #   shiny::eventReactive(dataMerge(), {
+  #     fxn_figureTitle(azmetStation = input$azmetStation)
+  #   })
+  # 
+  # pageSupportText <- 
+  #   shiny::eventReactive(dataMerge(), {
+  #     fxn_pageSupportText(
+  #       azmetStation = input$azmetStation,
+  #       startDate = input$plantingDate, 
+  #       endDate = input$endDate, 
+  #       timeStep = "Daily"
+  #     )
+  #   })
   
   
   # Outputs -----
   
-  output$figure <- 
-    plotly::renderPlotly({
-      figure()
-    })
-  
-  output$pageSupportText <- 
-    shiny::renderUI({
-      pageSupportText()
-    })
-  
-  output$figureFooter <- 
-    shiny::renderUI({
-      figureFooter()
-    })
-  
-  output$figureHelpText <- 
-    shiny::renderUI({
-      figureHelpText()
-    })
-  
-  output$figureSummary <- 
-    shiny::renderUI({
-      figureSummary()
-    })
-  
-  output$figureTitle <- 
-    shiny::renderUI({
-      figureTitle()
-    })
+  # output$figure <- 
+  #   plotly::renderPlotly({
+  #     figure()
+  #   })
+  # 
+  # output$pageSupportText <- 
+  #   shiny::renderUI({
+  #     pageSupportText()
+  #   })
+  # 
+  # output$figureFooter <- 
+  #   shiny::renderUI({
+  #     figureFooter()
+  #   })
+  # 
+  # output$figureHelpText <- 
+  #   shiny::renderUI({
+  #     figureHelpText()
+  #   })
+  # 
+  # output$figureSummary <- 
+  #   shiny::renderUI({
+  #     figureSummary()
+  #   })
+  # 
+  # output$figureTitle <- 
+  #   shiny::renderUI({
+  #     figureTitle()
+  #   })
 }
 
 
